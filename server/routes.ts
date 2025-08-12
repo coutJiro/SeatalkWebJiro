f552396db6d15f26bb974d0d6d903f8679024837
coutJiro <malinaojerome151@gmail.com>
2025-08-13 03:17:41 +0800

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("API is working!");
+  res.send("API is working");
 });
 
 export default router;
