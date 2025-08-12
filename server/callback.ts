b1735716fecd405458cd17ff8c91843239aba85c
coutJiro <malinaojerome151@gmail.com>
2025-08-13 02:30:19 +0800

    Update callback.ts

--- server/callback.ts
+++ server/callback.ts
@@ -2,19 +2,9 @@ import { Router } from "express";
 
 const router = Router();
 
-// SeaTalk webhook POST
 router.post("/", (req, res) => {
-  console.log("📩 Received SeaTalk callback:", req.body);
-
-  // Respond quickly so SeaTalk doesn't time out
+  console.log("Received SeaTalk event:", req.body);
   res.status(200).send("OK");
-
-  // TODO: Add bot logic here
-});
-
-// Optional GET for debugging
-router.get("/", (req, res) => {
-  res.send("SeaTalk callback endpoint - use POST for events");
 });
 
 export default router;
